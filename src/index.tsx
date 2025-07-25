89c5c055b63c5b01f00fdb2bd838ed7ab67b1a61
GuilleFerres <guillermoferressanchez@gmail.com>
2025-07-25 10:59:09 +0200

    Feat():'Add react and different environment into the project'

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import logoImg from './content/lemoncode-logo.png';
+import './mystyles.scss';
+
+
+const img = document.createElement('img');
+img.src = logoImg;
+img.alt = 'Lemoncode Logo';
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <h1 className="textcolor">Hola mundo!</h1>
+        <div id="imgContainer"></div>
+    </React.StrictMode>
+);
+
+setTimeout(() => {
+    const imgContainer = document.getElementById('imgContainer');
+    if (imgContainer) {
+        imgContainer.appendChild(img);
+    }
+}, 300);
+
---
+++ webpack.dev.js
@@ -0,0 +1,44 @@
+import common from "./webpack.common.js";
+import path from 'path';
+import url from 'url';
+import { merge } from "webpack-merge";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+export default merge(common, {
+  mode: "development",  
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                exportLocalsConvention: "camelCase",
+                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    devMiddleware: {
+      stats: "errors-only",
+    },
+  },
+});
